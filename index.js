9022894bfe420bf1041bc9dee95f418ba39b7164
fazamumtaz <fazamumtazramadhan@gmail.com>
2025-10-06 17:34:24 +0700

    Video 2: hit path and give json response

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const app = express()
 const PORT = 8090
 
 app.get('/', (request, response) => {
-  response.json('halo dunia')
+  response.json({greeting: 'halo dunia'})
+})
+
+app.get('/movies', (request, response) => {
+  console.log('Aw path movies di-hit')
+  response.json({
+    data: "Ini list movie yang gua males ngetiknya"
+  })
 })
 
 app.listen(PORT, () => console.log(`Server berjalan di port ${PORT}`))
